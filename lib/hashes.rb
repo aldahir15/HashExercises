8e18a38f003148985239b53dd7e93d6ce9c1bc55
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-05-30 19:51:38 -0700

    All Hash Exercises Done!

--- lib/hashes.rb
+++ lib/hashes.rb
@@ -3,11 +3,13 @@
 # Define a method that, given a sentence, returns a hash of each of the words as
 # keys with their lengths as values. Assume the argument lacks punctuation.
 def word_lengths(str)
+  Hash[arr_str = str.split(" ").collect{|x| [x,x.length] }]
 end
 
 # Define a method that, given a hash with integers as values, returns the key
 # with the largest value.
 def greatest_key_by_val(hash)
+  hash.keys[hash.values.index(hash.values.max)]
 end
 
 # Define a method that accepts two hashes as arguments: an older inventory and a
@@ -18,11 +20,23 @@ end
 # update_inventory(march, april) => {rubies: 10, emeralds: 27, diamonds: 2,
 # moonstones: 5}
 def update_inventory(older, newer)
+  older.each{|key, value|
+    if newer.has_key?(key)
+      older[key] = newer[key]
+    end
+  }
+  newer.each{|key, value|
+    unless older.has_key?(key)
+      older[key] = value
+    end }
+  older
 end
 
 # Define a method that, given a word, returns a hash with the letters in the
 # word as keys and the frequencies of the letters as values.
 def letter_counts(word)
+  new_arr = word.split("")
+  Hash[new_arr.collect{|x| [x,new_arr.count(x)]}]
 end
 
 # MEDIUM
@@ -30,17 +44,40 @@ end
 # Define a method that, given an array, returns that array without duplicates.
 # Use a hash! Don't use the uniq method.
 def my_uniq(arr)
+  new_hash = Hash[arr.collect{|x| [x,nil]}]
+  new_hash.to_a.flatten.compact
 end
 
 # Define a method that, given an array of numbers, returns a hash with "even"
 # and "odd" as keys and the frequency of each parity as values.
 def evens_and_odds(numbers)
+  counter_odd = 0
+  counter_even = 0
+  Hash[numbers.collect{|x|
+    if x.odd?
+      [:odd, counter_odd += 1]
+    else
+      [:even, counter_even += 1]
+    end
+  }]
 end
 
 # Define a method that, given a string, returns the most common vowel. If
 # there's a tie, return the vowel that occurs earlier in the alphabet. Assume
 # all letters are lower case.
 def most_common_vowel(string)
+  vowels = ["a","e","i","o","u"]
+  new_arr = string.split("")
+  new_hash = Hash[new_arr.collect{|x|
+    if vowels.include?(x)
+      [x,new_arr.count(x)]
+    end
+  }]
+  if new_hash.values.count(new_hash.values.max) == 1
+    return new_hash.sort_by{|w,v| v }.reverse[0][0]
+  else
+    return new_hash.sort_by{|w,v| w }[0][0]
+  end
 end
 
 # HARD
@@ -53,6 +90,9 @@ end
 # fall_and_winter_birthdays(students_with_birthdays) => [ ["Bertie", "Dottie"],
 # ["Bertie", "Warren"], ["Dottie", "Warren"] ]
 def fall_and_winter_birthdays(students)
+  new_arr = []
+  students.each{|key, value| new_arr << key if value >= 7}
+  new_arr.combination(2).to_a
 end
 
 # Define a method that, given an array of specimens, returns the biodiversity
@@ -61,6 +101,8 @@ end
 # "cat", "cat"]) => 1 biodiversity_index(["cat", "leopard-spotted ferret",
 # "dog"]) => 9
 def biodiversity_index(specimens)
+  new_hash = Hash[specimens.collect{|x| [x,specimens.count(x)]}]
+  new_hash.keys.length**2*new_hash.values.min/new_hash.values.max
 end
 
 # Define a method that, given the string of a respectable business sign, returns
@@ -69,7 +111,21 @@ end
 # can_tweak_sign("We're having a yellow ferret sale for a good cause over at the
 # pet shop!", "Leopard ferrets forever yo") => true
 def can_tweak_sign?(normal_sign, vandalized_sign)
+  normal_hash = character_count(normal_sign)
+  vandalized_hash = character_count(vandalized_sign)
+  vandalized_hash.each{|key, value|
+    unless normal_hash.has_key?(key)
+      return false
+    else
+      unless normal_hash[key] >= vandalized_hash[key]
+        return false
+      end
+    end
+  }
+  return true
 end
 
 def character_count(str)
+  new_arr = str.delete(" ").downcase.split("")
+  Hash[new_arr.collect{|x| [x,new_arr.count(x)]}]
 end
